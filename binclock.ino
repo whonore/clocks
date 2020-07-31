b6c1a826427aed2c5cc6eead5d72552dfe0bc7ab
whonore <wolfhonore@gmail.com>
2020-07-31 12:31:44 -0400

    Allow timing with micros

--- binclock.ino
+++ binclock.ino
@@ -1,3 +1,5 @@
+#define STATIC_ASSERT(name, test) typedef char assert_##name[(!!(test)) ? 1 : -1]
+
 #ifndef DEBUG
 #define DEBUG 0
 #endif
@@ -10,34 +12,55 @@ static char debug[DEBUG_SZ];
 #define DPRINTF(...) do {} while (0)
 #endif
 
-#define ULONG_MAX ((unsigned long) (-1))
-// How far from true msecs the clock is. E.g. 0.2% slow = -2/1000 = -2ms offset.
-#define CLOCK_DRIFT_PER_THOU (-2)
-#define TICKS_PER_SEC (1000 + CLOCK_DRIFT_PER_THOU)
-#define TICKS_MAX ((unsigned long) TICKS_PER_SEC * 60 * 60 * 24)
+#define USECS 1000000
+#define MSECS 1000
+
+#ifndef TICK_UNIT
+#define TICK_UNIT USECS
+#endif
+
+#if TICK_UNIT == USECS
+#define TICK() micros()
+#elif TICK_UNIT == MSECS
+#define TICK() millis()
+#else
+#error "TICK_UNIT must be USECS or MSECS"
+#endif
+
+typedef uint64_t ticks_t;
+#define TICKS_MAX ((ticks_t) (-1))
+// How far from true TICK_UNIT the clock is.
+// E.g. 0.15% slow = -1500/1000000 = -1.5ms offset.
+#define CLOCK_DRIFT_PER_MIL (-1500)
+#define CLOCK_DRIFT_TICKS (CLOCK_DRIFT_PER_MIL / (1000000 / TICK_UNIT))
+#define TICKS_PER_SEC ((ticks_t) TICK_UNIT + CLOCK_DRIFT_TICKS)
+#define TICKS_PER_DAY ((ticks_t) TICKS_PER_SEC * 60 * 60 * 24)
+STATIC_ASSERT(ticks_correct, TICKS_PER_DAY / TICKS_PER_SEC == (ticks_t) 60 * 60 * 24);
 
 struct time_t {
-    unsigned int secs;
-    unsigned int mins;
-    unsigned int hours;
+    byte secs;
+    byte mins;
+    byte hours;
 };
 
+typedef byte pin_t;
+
 // LEDs
-const static int hours[] = {8, 9, 10, 11, 12};
+const static pin_t hours[] = {8, 9, 10, 11, 12};
 #define NHOURS (sizeof(hours) / sizeof(hours[0]))
-const static int mins[] = {2, 3, 4, 5, 6, 7};
+const static pin_t mins[] = {2, 3, 4, 5, 6, 7};
 #define NMINS (sizeof(mins) / sizeof(mins[0]))
-const static int secs[] = {A0, A1, A2, A3, A4, A5};
+const static pin_t secs[] = {A0, A1, A2, A3, A4, A5};
 #define NSECS (sizeof(secs) / sizeof(secs[0]))
 
 // Buttons
-const static int incHour = 0;
-const static int incMin = 13;
+const static pin_t incHour = 0;
+const static pin_t incMin = 13;
 
-static unsigned long prev_ticks = 0;
-static unsigned long total_ticks = 0;
-static int last_hour_st = HIGH;
-static int last_min_st = LOW;
+static byte last_hour_st = HIGH;
+static byte last_min_st = LOW;
+static ticks_t prev_ticks = 0;
+static ticks_t total_ticks = 0;
 static struct time_t off = {.secs = 0, .mins = 0, .hours = 0};
 
 void setup() {
@@ -60,9 +83,9 @@ void setup() {
 
 void loop() {
     struct time_t time;
-    unsigned long ticks = millis();
-    unsigned long ellapsed =
-        prev_ticks < ticks ? ticks - prev_ticks : ticks + (ULONG_MAX - prev_ticks);
+    ticks_t ticks = TICK();
+    ticks_t ellapsed =
+        prev_ticks < ticks ? ticks - prev_ticks : ticks + (TICKS_MAX - prev_ticks);
     bool hour_pressed = pressed(incHour, &last_hour_st, LOW);
     bool min_pressed = pressed(incMin, &last_min_st, HIGH);
     bool at_second = (TICKS_PER_SEC <= ellapsed);
@@ -80,7 +103,7 @@ void loop() {
     }
 
     if (at_second) {
-        total_ticks = (total_ticks + ellapsed) % TICKS_MAX;
+        total_ticks = (total_ticks + ellapsed) % TICKS_PER_DAY;
         prev_ticks = ticks;
     }
 
@@ -94,23 +117,23 @@ void loop() {
     }
 }
 
-static bool pressed(unsigned int button, int *old, int active) {
+static bool pressed(pin_t button, byte *old, byte active) {
     int st = digitalRead(button);
     int st_old = *old;
     *old = st;
     return (st == active && st_old != active);
 }
 
-static void ticksToTime(struct time_t *time, unsigned long ticks) {
-    unsigned long sec = time->secs + (ticks / TICKS_PER_SEC);
-    unsigned long min = time->mins + (sec / 60);
-    unsigned long hr = time->hours + (min / 60);
+static void ticksToTime(struct time_t *time, ticks_t ticks) {
+    unsigned int sec = time->secs + (ticks / TICKS_PER_SEC);
+    unsigned int min = time->mins + (sec / 60);
+    unsigned int hr = time->hours + (min / 60);
     time->hours = hr % 24;
     time->mins = min % 60;
     time->secs = sec % 60;
 }
 
-static void dispTime(const int *leds, unsigned int val, unsigned int nleds) {
+static void dispTime(const pin_t *leds, unsigned int val, unsigned int nleds) {
     for (unsigned int i = 0; i < nleds; i++) {
         digitalWrite(leds[i], bitRead(val, i) ? HIGH : LOW);
     }
