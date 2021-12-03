355c43fbf66b87c80c01915d01b6cefffe4af912
whonore <wolfhonore@gmail.com>
2021-12-03 14:30:03 -0500

    Add library stubs for AccelStepper and TimerInterrupt

---
+++ simulator/arduino/TimerInterrupt.h
@@ -0,0 +1,34 @@
+#ifndef TIMERINTERRUPT_H
+#define TIMERINTERRUPT_H
+
+#include <arduino.h>
+
+typedef void (*timer_callback)();
+
+enum {
+    HW_TIMER_0 = 0,
+    HW_TIMER_1,
+    HW_TIMER_2,
+    HW_TIMER_3,
+    HW_TIMER_4,
+    HW_TIMER_5,
+    NUM_HW_TIMERS
+};
+
+class TimerInterrupt {
+    public:
+        TimerInterrupt(uint8_t UNUSED(timerNo)) { }
+
+        void init() { }
+
+        bool attachInterruptInterval(unsigned long UNUSED(interval),
+                                     timer_callback UNUSED(callback),
+                                     unsigned long UNUSED(duration) = 0) {
+            return true;
+        }
+
+};
+
+TimerInterrupt ITimer1(HW_TIMER_1);
+
+#endif /* TIMERINTERRUPT_H */
