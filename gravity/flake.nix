67168949210d63644807af2b906a91a080cf0abc
whonore <wolfhonore@gmail.com>
2022-03-25 13:52:57 -0400

    Update flake

--- gravity/flake.nix
+++ gravity/flake.nix
@@ -1,10 +1,16 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      py-packages = python-packages: with python-packages; [ pillow ];
-      python-png = pkgs.python3.withPackages py-packages;
-    in { devShell.x86_64-linux = pkgs.mkShell { packages = [ python-png ]; }; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    py-packages = python-packages: with python-packages; [pillow];
+    python-png = pkgs.python3.withPackages py-packages;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [python-png];
+    };
+  };
 }
