6a42f87291d434e5f42a5f5531c392dcfc3c3569
whonore <wolfhonore@gmail.com>
2022-07-25 14:46:37 -0400

    build: add more archs to gravity's flake.nix

--- clocks/gravity/flake.nix
+++ clocks/gravity/flake.nix
@@ -5,12 +5,17 @@
     self,
     nixpkgs,
   }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    py-packages = python-packages: with python-packages; [pillow];
-    python-png = pkgs.python3.withPackages py-packages;
+    env = system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      py-packages = python-packages: with python-packages; [pillow];
+      python-png = pkgs.python3.withPackages py-packages;
+    in
+      pkgs.mkShell {packages = [python-png];};
   in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [python-png];
-    };
+    devShells.aarch64-darwin.default = env "aarch64-darwin";
+    devShells.aarch64-linux.default = env "aarch64-linux";
+    devShells.i686-linux.default = env "i686-linux";
+    devShells.x86_64-darwin.default = env "x86_64-darwin";
+    devShells.x86_64-linux.default = env "x86_64-linux";
   };
 }
